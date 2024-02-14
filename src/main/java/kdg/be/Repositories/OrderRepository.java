d71ffb73ee688bff8a30f49501c65d722790bae6
Eliah Verbeemen <eliah.verbeemen@student.kdg.be>
2024-02-14 17:37:05 +0000

    Update 21 files
    - /.idea/compiler.xml
    - /src/main/resources/application.properties
    - /src/test/java/kdg/be/ClientApiApplicationTests.java
    - /src/main/java/kdg/be/Controllers/BeheerdersController.java
    - /src/main/java/kdg/be/Controllers/KlantenController.java
    - /src/main/java/kdg/be/Modellen/Klant.java
    - /src/main/java/kdg/be/Modellen/Loyaliteitsklasse.java
    - /src/main/java/kdg/be/Modellen/Order.java
    - /src/main/java/kdg/be/Modellen/Product.java
    - /src/main/java/kdg/be/Repositories/KlantRepository.java
    - /src/main/java/kdg/be/Repositories/OrderRepository.java
    - /src/main/java/kdg/be/Repositories/ProductRepository.java
    - /src/main/java/kdg/be/SerializatieHelpers/SetSerialisatie.java
    - /src/main/java/kdg/be/ClientApiApplication.java
    - /.gitignore
    - /build.gradle
    - /compose.yaml
    - /gradlew
    - /gradlew.bat
    - /HELP.md
    - /settings.gradle

--- src/main/java/kdg/be/Repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package kdg.be.Repositories;
-
-import kdg.be.Modellen.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
