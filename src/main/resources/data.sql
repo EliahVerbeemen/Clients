9e72c5dd1e82732e40b401922aa3c1a661e7901c
Eliah Verbeemen <eliah.verbeemen@student.kdg.be>
2024-05-08 03:26:51 +0000

    filteren&Seeding

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+SET search_path TO client,public;
+insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (0,0,1,'brons');
+insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (1000,0.05,2,'zilver');
+insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (5000,0.10,3,'goud');
+insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (10000,0.20,4,'platina')
+
+
