d27870c3eca13c466201ac2093dead6639178607
Eliah Verbeemen <eliah.verbeemen@student.kdg.be>
2024-05-13 11:17:19 +0000

    Update 65 files
    - /src/main/resources/application.properties
    - /src/main/resources/data.sql
    - /src/main/java/kdg/be/Config/Configuration.java
    - /src/main/java/kdg/be/Config/SecurityConfig.java
    - /src/main/java/kdg/be/Controllers/ClientController.java
    - /src/main/java/kdg/be/Controllers/ClientManagerController.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitConfiguration.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitListener.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitSender.java
    - /src/main/java/kdg/be/Repositories/BatchRepository.java
    - /src/main/java/kdg/be/Repositories/ClientRepository.java
    - /src/main/java/kdg/be/Repositories/LoyalityClassRepository.java
    - /src/main/java/kdg/be/Repositories/OrderRepository.java
    - /src/main/java/kdg/be/Repositories/ProductRepository.java
    - /src/main/java/kdg/be/SerializatieHelpers/SetSerialisatie.java
    - /src/main/java/kdg/be/Xml/Account.java
    - /src/main/java/kdg/be/Xml/Item.java
    - /src/main/java/kdg/be/Xml/Items.java
    - /src/main/java/kdg/be/Xml/PurchaseOrder.java
    - /src/main/java/kdg/be/Xml/xmltest.java
    - /src/main/java/kdg/be/testjes/BeestigenVanOrder.png
    - /src/main/java/kdg/be/testjes/ClientNotFoundException.java
    - /src/main/java/kdg/be/testjes/ProductNotFoundException.java
    - /src/main/java/kdg/be/testjes/img.png
    - /src/main/java/kdg/be/testjes/img_1.png
    - /src/main/java/kdg/be/testjes/img_2.png
    - /src/main/java/kdg/be/testjes/img_3.png
    - /src/main/java/kdg/be/testjes/img_4.png
    - /src/main/java/kdg/be/testjes/img_5.png
    - /src/main/java/kdg/be/testjes/img_6.png
    - /src/main/java/kdg/be/testjes/img_7.png
    - /src/main/java/kdg/be/testjes/oefenen.md
    - /src/test/java/kdg/be/Controllers/ClientControllerTests.java
    - /src/test/java/kdg/be/Controllers/ClientServiceControllerTests.java
    - /src/test/java/kdg/be/ClientApiApplicationTests.java
    - /src/test/java/kdg/be/ClientsApplicationTests.java
    - /src/main/java/kdg/be/Modellen/Enums/ClientType.java
    - /src/main/java/kdg/be/Modellen/Enums/OrderStatus.java
    - /src/main/java/kdg/be/Services/Interfaces/IBatchManager.java
    - /src/main/java/kdg/be/Services/Interfaces/IClientManager.java
    - /src/main/java/kdg/be/Services/Interfaces/ILoyalityClassManager.java
    - /src/main/java/kdg/be/Services/Interfaces/IOrderManager.java
    - /src/main/java/kdg/be/Services/Interfaces/IProductManager.java
    - /src/main/java/kdg/be/Services/BatchManager.java
    - /src/main/java/kdg/be/Services/ClientService.java
    - /src/main/java/kdg/be/Services/FilterService.java
    - /src/main/java/kdg/be/Services/LoyaltyClassService.java
    - /src/main/java/kdg/be/Services/OrderService.java
    - /src/main/java/kdg/be/Services/ProductService.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/ClientDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/LoyaltyClassDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/OrderDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ClientWithProducts.java
    - /src/main/java/kdg/be/Modellen/DTO/ProductFromBakery.java
    - /src/main/java/kdg/be/Modellen/Batch.java
    - /src/main/java/kdg/be/Modellen/Client.java
    - /src/main/java/kdg/be/Modellen/LoyaltyClass.java
    - /src/main/java/kdg/be/Modellen/Order.java
    - /src/main/java/kdg/be/Modellen/Product.java
    - /src/main/java/kdg/be/Modellen/ProductState.java
    - /src/main/java/kdg/be/Modellen/Test.java
    - /src/main/java/kdg/be/AfterTenOClockError.java
    - /src/main/java/kdg/be/ClientApiApplication.java
    - /src/main/java/kdg/be/ClientsApplication.java
    - /src/main/java/kdg/be/errorHandler.java

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-SET search_path TO client,public;
-DELETE FROM loyality_classes;
-insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (0,0,1,'brons');
-insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (1000,0.05,2,'zilver');
-insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (5000,0.10,3,'goud');
-insert into loyality_classes (minimum_points,reduction,loyality_class_id,name) values (10000,0.20,4,'platina')
-
-
