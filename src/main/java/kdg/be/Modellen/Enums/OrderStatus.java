e8c9a20904da07ea5a852c76024a5feb3ed0a99e
Eliah Verbeemen <eliah.verbeemen@student.kdg.be>
2024-06-04 10:21:05 +0000

    Update 52 files
    - /src/main/resources/application.properties
    - /src/main/resources/data.sql
    - /src/main/java/kdg/be/Config/Configuration.java
    - /src/main/java/kdg/be/Config/SecurityConfig.java
    - /src/main/java/kdg/be/Controller/ClientController.java
    - /src/main/java/kdg/be/Controller/ClientManagerController.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitConfiguration.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitListener.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitSender.java
    - /src/main/java/kdg/be/Repositories/ClientRepository.java
    - /src/main/java/kdg/be/Repositories/LoyaltyClassRepository.java
    - /src/main/java/kdg/be/Repositories/OrderRepository.java
    - /src/main/java/kdg/be/Repositories/ProductRepository.java
    - /src/main/java/kdg/be/SerializationHelpers/OrderSerializer.java
    - /src/main/java/kdg/be/SerializationHelpers/ProductDeserializer.java
    - /src/main/java/kdg/be/SerializationHelpers/PurchaseOrderSerializer.java
    - /src/main/java/kdg/be/Xml/Account.java
    - /src/main/java/kdg/be/Xml/Item.java
    - /src/main/java/kdg/be/Xml/Items.java
    - /src/main/java/kdg/be/Xml/PurchaseOrder.java
    - /src/test/java/kdg/be/Controller/ClientControllerTest.java
    - /src/test/java/kdg/be/Controller/ClientManagerControllerTests.java
    - /src/main/java/kdg/be/Modellen/Enums/ClientType.java
    - /src/main/java/kdg/be/Modellen/Enums/OrderStatus.java
    - /src/main/java/kdg/be/Services/Interfaces/IClientService.java
    - /src/main/java/kdg/be/Services/Interfaces/ILoyaltyClassService.java
    - /src/main/java/kdg/be/Services/Interfaces/IOrderService.java
    - /src/main/java/kdg/be/Services/Interfaces/IProductService.java
    - /src/main/java/kdg/be/Services/ClientService.java
    - /src/main/java/kdg/be/Services/FilterService.java
    - /src/main/java/kdg/be/Services/LoyaltyClassService.java
    - /src/main/java/kdg/be/Services/OrderService.java
    - /src/main/java/kdg/be/Services/ProductService.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/ClientDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/LoyaltyClassDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/OrderDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ControllerDTO/PurchaseOrderDTO.java
    - /src/main/java/kdg/be/Modellen/DTO/ClientWithProducts.java
    - /src/main/java/kdg/be/Modellen/DTO/ProductFromBakery.java
    - /src/main/java/kdg/be/Modellen/Client.java
    - /src/main/java/kdg/be/Modellen/LoyaltyClass.java
    - /src/main/java/kdg/be/Modellen/Order.java
    - /src/main/java/kdg/be/Modellen/Product.java
    - /src/main/java/kdg/be/Modellen/ProductState.java
    - /src/main/java/kdg/be/ClientsApplication.java
    - /.gitignore
    - /build.gradle
    - /compose.yaml
    - /gradlew
    - /gradlew.bat
    - /settings.gradle
    - /zelfreflectie.md

--- src/main/java/kdg/be/Modellen/Enums/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package kdg.be.Modellen.Enums;
-
-public enum OrderStatus {
-    NOT_CONFIRMED,
-    CONFIRMED,
-    CANCELLED
-}
--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-SET search_path TO client,public;
-DELETE FROM loyalty_class;
-insert into loyalty_class (minimum_points,reduction,loyalty_class_id,name) values (0,0,1,'bronze');
-insert into loyalty_class (minimum_points,reduction,loyalty_class_id,name) values (1000,0.05,2,'silver');
-insert into loyalty_class(minimum_points,reduction,loyalty_class_id,name) values (5000,0.10,3,'gold');
-insert into loyalty_class (minimum_points,reduction,loyalty_class_id,name) values (10000,0.20,4,'platinum');
-
-
